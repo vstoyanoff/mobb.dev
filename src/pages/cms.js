baf385b41fd5ab25be5bc09a09bd646918b6e009
Veselin Stoyanov <veselin@stan.vision>
2020-05-29 21:13:06 +0300

    feat: Add option to set featured articles

--- src/pages/cms.js
+++ src/pages/cms.js
@@ -54,6 +54,39 @@ const StyledActions = styled.div`
   }
 `;
 
+const StyledList = styled.div`
+  flex: 1 0 48%;
+  max-width: 48%;
+  max-height: 60vh;
+  overflow: auto;
+  box-shadow: 6px 6px 14px 0 rgba(20, 20, 20, 0.2),
+    -8px -8px 18px 0 rgba(255, 255, 255, 0.55);
+  border-radius: 30px;
+  padding: 20px;
+
+  .dark-mode & {
+    box-shadow: 6px 6px 14px 0 rgba(20, 20, 20, 0.2),
+      -8px -8px 18px 0 rgba(20, 20, 20, 0.55);
+  }
+
+  @media (max-width: 767px) {
+    flex: 1 0 100%;
+    max-width: 100%;
+    margin-bottom: 30px;
+  }
+
+  button {
+    appearance: none;
+    text-align: left;
+    border: none;
+    padding: 0;
+    margin: 0;
+    background: transparent;
+    text-decoration: underline;
+    cursor: pointer;
+  }
+`;
+
 const Cms = () => {
   /**
    * State
@@ -61,6 +94,8 @@ const Cms = () => {
   const [auth, setAuth] = React.useState(false);
   const [loading, setLoading] = React.useState(false);
   const [mode, setMode] = React.useState('');
+  const [allArticles, setAllArticles] = React.useState([]);
+  const [featured, setFeatured] = React.useState([]);
   const [drafts, setDrafts] = React.useState([]);
   const [draftId, setDraftId] = React.useState(null);
   const [data, setData] = React.useState({
@@ -89,13 +124,11 @@ const Cms = () => {
         firebase
           .database()
           .ref('/articles')
-          .once('value')
-          .then(data => {
-            const articles = Object.values(data.val());
-            const filtered = articles.filter(
-              article => article.state === 'draft'
-            );
-            setDrafts(filtered);
+          .on('value', snapshot => {
+            const articles = Object.values(snapshot.val());
+            setAllArticles(articles);
+            setFeatured(articles.filter(article => article.featured));
+            setDrafts(articles.filter(article => article.state === 'draft'));
           });
       }
     });
@@ -212,6 +245,14 @@ const Cms = () => {
     }).then(res => setLoading(false));
   };
 
+  const handleFeatured = (article, state) => {
+    const updated = { ...article, featured: state };
+    firebase
+      .database()
+      .ref(`articles/${updated.date}`)
+      .set(updated);
+  };
+
   return (
     <Layout>
       {loading && <LoadingOverlay />}
@@ -238,6 +279,13 @@ const Cms = () => {
                 onClick={() => setMode('edit')}
               >
                 Edit existing
+              </StyledButton>
+
+              <StyledButton
+                active={mode === 'featured'}
+                onClick={() => setMode('featured')}
+              >
+                Featured
               </StyledButton>
 
               <StyledButton onClick={handleDeploy}>Deploy changes</StyledButton>
@@ -358,6 +406,37 @@ const Cms = () => {
                   </>
                 )}
               </>
+            )}
+
+            {mode === 'featured' && (
+              <div
+                style={{
+                  display: 'flex',
+                  justifyContent: 'space-between',
+                  marginTop: 40,
+                  flexWrap: 'wrap',
+                }}
+              >
+                <StyledList>
+                  {allArticles.map(article => (
+                    <p key={article.date}>
+                      <button onClick={() => handleFeatured(article, true)}>
+                        {article.title}
+                      </button>
+                    </p>
+                  ))}
+                </StyledList>
+
+                <StyledList>
+                  {featured.map(article => (
+                    <p key={article.date}>
+                      <button onClick={() => handleFeatured(article, false)}>
+                        {article.title}
+                      </button>
+                    </p>
+                  ))}
+                </StyledList>
+              </div>
             )}
           </div>
         </section>
